8df6998f78dabe9cbf78bbcabe921c91d026ba09
Blupilot <Blupilot@BLUPILOT-PC>
2017-05-24 22:29:02 -0400

    Added BlockBasic blocks to creative tab and made unlocalized names in "tile.modName.blockName.name" format

--- src/main/java/com/appiogenesis/rpg/block/BlockBasic.java
+++ src/main/java/com/appiogenesis/rpg/block/BlockBasic.java
@@ -3,6 +3,7 @@ package com.appiogenesis.rpg.block;
 import com.appiogenesis.rpg.RPG;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -10,8 +11,9 @@ public class BlockBasic extends Block{
 
 	public BlockBasic(String name) {
 		super(Material.ROCK);
-		setUnlocalizedName(RPG.modId + name);
+		setUnlocalizedName(RPG.modId + "." + name);
 		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MISC);
 		GameRegistry.register(this);
 		GameRegistry.register(new ItemBlock(this), getRegistryName());
 	}
