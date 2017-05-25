de25c1775d24c263113927a8abdb0b9895037f48
Blupilot <Blupilot@BLUPILOT-PC>
2017-05-25 16:34:41 -0400

    Added test item

--- src/main/java/com/appiogenesis/rpg/init/ModItems.java
+++ src/main/java/com/appiogenesis/rpg/init/ModItems.java
@@ -1,9 +1,13 @@
 package com.appiogenesis.rpg.init;
 
+import com.appiogenesis.rpg.item.ItemBasic;
+
 public class ModItems {
 
-	public static void init() {
+	public static ItemBasic testItem;
 
+	public static void init() {
+		testItem = new ItemBasic("testItem");
 	}
 
 }
--- src/main/java/com/appiogenesis/rpg/item/ItemBasic.java
+++ src/main/java/com/appiogenesis/rpg/item/ItemBasic.java
@@ -1,9 +1,17 @@
 package com.appiogenesis.rpg.item;
 
+import com.appiogenesis.rpg.RPG;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ItemBasic extends Item{
 
-
+	public ItemBasic(String name) {
+		setUnlocalizedName(RPG.modId + "." + name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MISC);
+		GameRegistry.register(this);
+	}
 
 }
