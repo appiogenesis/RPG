d85d026c83dc02cb81f4c7dda9d30c401ad09470
Blupilot <Blupilot@BLUPILOT-PC>
2017-05-26 12:50:20 -0400

    Made test tile entity

--- src/main/java/com/appiogenesis/rpg/init/ModBlocks.java
+++ src/main/java/com/appiogenesis/rpg/init/ModBlocks.java
@@ -1,13 +1,16 @@
 package com.appiogenesis.rpg.init;
 
 import com.appiogenesis.rpg.block.BlockBasic;
+import com.appiogenesis.rpg.block.DataBlock;
 
 public class ModBlocks {
 
 	public static BlockBasic testBlock;
+	public static DataBlock dataBlock;
 
 	public static void init() {
 		testBlock = new BlockBasic("testBlock");
+		dataBlock = new DataBlock();
 	}
 
 }
